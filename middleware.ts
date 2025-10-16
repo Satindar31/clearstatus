0ead85b6f0c4ba9f8e6eacabdd085631717dbc5c
Satindar <73517520+Satindar31@users.noreply.github.com>
2025-10-16 19:12:24 +0530

    feat: refactor middleware to handle subdomain routing and rewrite requests

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,54 @@
 import { NextResponse, NextRequest } from "next/server";
 
+/*
+ * Copied this from my other project https://github.com/satindar31/openpages
+ */
+
 export async function middleware(req: NextRequest) {
-  let subdomain = null;
-  try {
-    const afterDoubleSlash = req.url.split("//")[1];
-    if (afterDoubleSlash) {
-      subdomain = afterDoubleSlash.split(".")[0];
-    } else {
-      // Handle URLs like 'localhost:3000' (no //)
-      subdomain = req.url.split(".")[0].replace(/^https?:\/?\/?/, "").split(":")[0];
-    }
-  } catch (e) {
-    subdomain = null;
-  }
-  if (subdomain === "www" || subdomain === "status") {
+  const { hostname, pathname } = req.nextUrl;
+  const baseDomain = process.env.APP_URL ?? "localhost";
+
+  // If the URL is simple BASE_URL, allow default behavior
+  if (hostname == baseDomain) {
     return NextResponse.next();
   }
-  const newUrl = `status/${subdomain}`;
-  console.log(newUrl);
-  return NextResponse.redirect(new URL(process.env.APP_URL!, newUrl));
+  // If not then if the URL is a subdomain like: user.openpages.blog do the following
+  else {
+    if (hostname.endsWith(baseDomain)) {
+      const segments = pathname.split("/");
+      // const blogId = segments[segments.length - 1];
+      const subdomain = hostname.replace(`.${baseDomain}`, "");
+      // Fetch the publication from the subdomain
+      // const publication = await findBlogByDomain(subdomain, false);
+
+      // if (publication) {
+      //   console.log("publication found");
+
+      // If the URL contains blogId, rewrite to the blog's dynamic route
+      //   if (blogId) {
+      req.nextUrl.pathname = `/status/${subdomain}`;
+      return NextResponse.rewrite(req.nextUrl);
+      //   }
+
+      // If no blogId, rewrite to the blog's main page
+      //   request.nextUrl.pathname = `/blog/${publication.id}`;
+      //   return NextResponse.rewrite(request.nextUrl);
+      // }
+      // Else return 404;
+      //     return NextResponse.rewrite(new URL("/404", request.url));
+      //   } else {
+      //     // Handle custom domains like: example.com
+      //     const blog = await findBlogByDomain(hostname, true);
+      //     if (blog) {
+      //       // Rewrite to the blog's dynamic route (e.g., `/blog/[id]`)
+      //       request.nextUrl.pathname = `/blog/${blog.id}`; // Adjust this to your routing
+      //       return NextResponse.rewrite(request.nextUrl);
+      //     }
+
+      //     // If no matching blog or domain, allow default behavior or return a 404
+      //     return NextResponse.next();
+    }
+  }
 }
 
 export const config = {
