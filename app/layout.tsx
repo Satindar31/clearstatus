c2d42c3459c6be6e45a39496a3c8c7753bd4aec5
Satindar <73517520+Satindar31@users.noreply.github.com>
2025-11-04 13:02:15 +0530

    feat: analytics using umami

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 import { Toaster } from "sonner";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({
       >
         {children}
         <Toaster />
+        <Script defer src={process.env.UMAMI_HOST} data-website-id={process.env.UMAMI_WEBSITE_ID} />
       </body>
     </html>
   );
